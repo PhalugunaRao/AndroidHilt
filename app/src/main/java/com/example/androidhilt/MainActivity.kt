827017d4086ba657b460d0dd5151cc763295594b
PhalgunaRao <phalu900@gmail.com>
2021-06-25 22:15:12 +0530

    Different scopes on hilt

--- app/src/main/java/com/example/androidhilt/MainActivity.kt
+++ app/src/main/java/com/example/androidhilt/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.androidhilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -14,27 +18,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         println(someClass.doAThing())
-        println(someClass.dothing())
+
     }
 }
 
-class  SomeClass
-@Inject constructor(
-    private val someOtherClass: SomeOtherClass
-){
+@AndroidEntryPoint
+class MyFragment :Fragment(){
+    @Inject
+    lateinit var someClass: SomeClass
+}
+
+@FragmentScoped
+class  SomeClass @Inject constructor(){
     fun doAThing():String{
         return "Look I did a thing!"
     }
-
-    fun dothing():String{
-        return someOtherClass.doSomeOtherThing()
-    }
 }
 
-class SomeOtherClass @Inject constructor(){
-    fun doSomeOtherThing():String{
-        return "Look i did some other thing!"
-    }
-}
