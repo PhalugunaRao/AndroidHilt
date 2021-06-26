66055811585981ba1eed714e327fbd35dc37ee2a
PhalgunaRao <phalu900@gmail.com>
2021-06-26 21:36:43 +0530

    hilt

--- app/src/main/java/com/example/androidhilt/MainActivity.kt
+++ app/src/main/java/com/example/androidhilt/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.androidhilt
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.constraintlayout.solver.widgets.analyzer.Dependency
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.scopes.ActivityScoped
-import dagger.hilt.android.scopes.FragmentScoped
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -35,7 +40,9 @@ constructor(
 
 class SomeInterfaceImpl
 @Inject
-constructor():SomeInterface{
+constructor(
+    private val someDependency: String
+):SomeInterface{
     override fun getAthing(): String {
         return "A Thing"
     }
@@ -44,4 +51,52 @@ constructor():SomeInterface{
 interface SomeInterface{
     fun getAthing():String
 }
+
+
+@InstallIn(ApplicationComponent::class)
+@Module
+class MyModule{
+
+    @Singleton
+    @Provides
+    fun provideSomeString():String{
+        return "some string"
+    }
+    @Singleton
+    @Provides
+    fun  provideSomeInterface(
+        someString:String
+    ):SomeInterface{
+        return SomeInterfaceImpl(someString)
+    }
+}
+
+
+/*
+//Binds
+@InstallIn(ApplicationComponent::class)
+@Module
+abstract class MyModule{
+    @Singleton
+    @Binds
+    abstract  fun bindSomeDependency(
+        someImpl:SomeInterfaceImpl
+    ):SomeInterface
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
